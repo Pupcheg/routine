5cb930e06d84109ca39a9593d26c483917322644
Pupcheg <santos43241@gmail.com>
2026-01-24 17:32:24 +0300

    feat: add EitherTest

---
+++ src/test/java/me/supcheg/routine/EitherTest.java
@@ -0,0 +1,79 @@
+package me.supcheg.routine;
+
+import org.junit.jupiter.api.Test;
+
+import static me.supcheg.routine.Either.left;
+import static me.supcheg.routine.Either.right;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EitherTest {
+
+    static final String LEFT = "_left";
+    static final String RIGHT = "_right";
+
+    static final String MAP = "_map";
+    static final String FOLD = "_fold";
+    static final String FLAT_MAP = "_flat_map";
+
+    @Test
+    void map() {
+        assertThat(left(LEFT).map(l -> l + MAP + LEFT, r -> r + MAP + RIGHT)).isEqualTo(left(LEFT + MAP + LEFT));
+        assertThat(right(RIGHT).map(l -> l + MAP + LEFT, r -> r + MAP + RIGHT)).isEqualTo(right(RIGHT + MAP + RIGHT));
+    }
+
+    @Test
+    void mapLeft() {
+        assertThat(left(LEFT).mapLeft(l -> l + MAP)).isEqualTo(left(LEFT + MAP));
+        assertThat(right(RIGHT).mapLeft(l -> l + MAP)).isEqualTo(right(RIGHT));
+    }
+
+    @Test
+    void mapRight() {
+        assertThat(left(LEFT).mapRight(r -> r + MAP)).isEqualTo(left(LEFT));
+        assertThat(right(RIGHT).mapRight(r -> r + MAP)).isEqualTo(right(RIGHT + MAP));
+    }
+
+    @Test
+    void flatMap() {
+        assertThat(left(LEFT).flatMap(l -> right(l + FLAT_MAP + LEFT), r -> left(r + FLAT_MAP + RIGHT)))
+                .isEqualTo(right(LEFT + FLAT_MAP + LEFT));
+        assertThat(right(RIGHT).flatMap(l -> right(l + FLAT_MAP + LEFT), r -> left(r + FLAT_MAP + RIGHT)))
+                .isEqualTo(left(RIGHT + FLAT_MAP + RIGHT));
+    }
+
+    @Test
+    void flatMapLeft() {
+        assertThat(left(LEFT).flatMapLeft(l -> right(l + FLAT_MAP + LEFT))).isEqualTo(right(LEFT + FLAT_MAP + LEFT));
+        assertThat(right(RIGHT).flatMapLeft(l -> right(l + FLAT_MAP + LEFT))).isEqualTo(right(RIGHT));
+    }
+
+    @Test
+    void flatMapRight() {
+        assertThat(left(LEFT).flatMapRight(r -> left(r + FLAT_MAP + RIGHT))).isEqualTo(left(LEFT));
+        assertThat(right(RIGHT).flatMapRight(r -> left(r + FLAT_MAP + RIGHT))).isEqualTo(left(RIGHT + FLAT_MAP + RIGHT));
+    }
+
+    @Test
+    void flip() {
+        assertThat(left(LEFT).flip()).isEqualTo(right(LEFT));
+        assertThat(right(RIGHT).flip()).isEqualTo(left(RIGHT));
+    }
+
+    @Test
+    void fold() {
+        assertThat((String) left(LEFT).fold(l -> l + FOLD + LEFT, r -> r + FOLD + RIGHT)).isEqualTo(LEFT + FOLD + LEFT);
+        assertThat((String) right(RIGHT).fold(l -> l + FOLD + LEFT, r -> r + FOLD + RIGHT)).isEqualTo(RIGHT + FOLD + RIGHT);
+    }
+
+    @Test
+    void optionalLeft() {
+        assertThat(left(LEFT).left()).contains(LEFT);
+        assertThat(right(RIGHT).left()).isEmpty();
+    }
+
+    @Test
+    void optionalRight() {
+        assertThat(left(LEFT).right()).isEmpty();
+        assertThat(right(RIGHT).right()).contains(RIGHT);
+    }
+}
