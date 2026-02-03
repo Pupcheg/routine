73a8f6f49e14c691b0b3eeb762df12225e14d4cf
Pupcheg <santos43241@gmail.com>
2026-02-03 20:10:28 +0300

    feat: add javadoc for package-info

--- src/main/java/me/supcheg/routine/package-info.java
+++ src/main/java/me/supcheg/routine/package-info.java
@@ -1,3 +1,10 @@
+/// Core generic types for everyday Java use.
+///
+/// Includes immutable, minimalistic containers, designed for clear semantics and seamless integration with the Java
+/// Stream API
+///
+/// @see me.supcheg.routine.Pair
+/// @see me.supcheg.routine.Either
 @NullMarked
 package me.supcheg.routine;
 
