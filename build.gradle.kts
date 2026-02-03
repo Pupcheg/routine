0aa10edea924d5a3b70a3eb4462ecbf6ce35f349
Pupcheg <santos43241@gmail.com>
2026-02-03 19:32:02 +0300

    feat: update dependencies' versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ repositories {
 dependencies {
     implementation("org.jspecify:jspecify:1.0.0")
 
-    testImplementation("org.assertj:assertj-core:3.27.6")
+    testImplementation("org.assertj:assertj-core:3.27.7")
     testImplementation("org.mockito:mockito-junit-jupiter:5.21.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
